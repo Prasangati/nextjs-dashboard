e246a3bb3c054e4dd27fbfface65f53c43c7a8b2
prasangatiwari <prasangahere@gmail.com>
2025-05-19 00:46:32 -0400

    Authentication and metadata error resolved

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
